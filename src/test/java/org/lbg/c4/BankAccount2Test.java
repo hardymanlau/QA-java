b23029897742c79b40f914e3005d4f284e41ec91
Lauren Hardyman <hardymanlau@gmail.com>
2024-10-29 15:07:07 +0000

    created more tests for BankAccount2

--- src/test/java/org/lbg/c4/BankAccount2Test.java
+++ src/test/java/org/lbg/c4/BankAccount2Test.java
@@ -25,4 +25,93 @@ public class BankAccount2Test {
     }
 
 
+    @Test
+    public void onCredit_balance_increased_by_amount() {
+        // not camelCase as easier to read unless told to use camelCase
+
+        // all tests have 3 parts, the 3 As - arrange, act, assert
+
+        // arrange - in arrange, always call what testing 'cut'
+        double openingBalance = 34.75;
+        BankAccount2 cut = new BankAccount2(openingBalance);    // avoids hard coding numbers
+        double expectedResult = 39.75;
+
+
+        // act
+        cut.credit(5);
+        double actualResult = cut.getBalance();
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+
+
+    @Test
+    public void onDebit_balance_decreased_by_amount() {
+        // not camelCase as easier to read unless told to use camelCase
+
+        // all tests have 3 parts, the 3 As - arrange, act, assert
+
+        // arrange - in arrange, always call what testing 'cut'
+        double openingBalance = 34.75;
+        BankAccount2 cut = new BankAccount2(openingBalance);    // avoids hard coding numbers
+        double expectedResult = 29.75;
+
+
+        // act
+        cut.debit(5);
+        double actualResult = cut.getBalance();
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+
+    @Test
+    public void onDebit_balance_fails() {
+        // not camelCase as easier to read unless told to use camelCase
+
+        // all tests have 3 parts, the 3 As - arrange, act, assert
+
+        // arrange - in arrange, always call what testing 'cut'
+        double openingBalance = 34.75;
+        BankAccount2 cut = new BankAccount2(openingBalance);    // avoids hard coding numbers
+        double expectedResult = 34.75;
+
+
+        // act
+        cut.debit(40);
+        double actualResult = cut.getBalance();
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+
+    @Test
+    public void onCredit_balance_fails() {
+        // not camelCase as easier to read unless told to use camelCase
+
+        // all tests have 3 parts, the 3 As - arrange, act, assert
+
+        // arrange - in arrange, always call what testing 'cut'
+        double openingBalance = 34.75;
+        BankAccount2 cut = new BankAccount2(openingBalance);    // avoids hard coding numbers
+        double expectedResult = 34.75;
+
+
+        // act
+        cut.credit(-5);
+        double actualResult = cut.getBalance();
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+
 }
