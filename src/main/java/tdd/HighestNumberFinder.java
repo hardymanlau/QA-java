6973b02a9cf15ce9f30d93d0e9dd0235eae066cb
Lauren Hardyman <hardymanlau@gmail.com>
2024-10-30 16:53:15 +0000

    second test done, two items in array

--- src/main/java/tdd/HighestNumberFinder.java
+++ src/main/java/tdd/HighestNumberFinder.java
@@ -2,6 +2,7 @@ package tdd;
 
 public class HighestNumberFinder {
     public int findHighestNumber(int[] input) {
+        
         return input[0];
     }
 }
--- src/test/java/tdd/HighestNumberFinderTest.java
+++ src/test/java/tdd/HighestNumberFinderTest.java
@@ -21,4 +21,36 @@ public class HighestNumberFinderTest {
 
     }
 
+    @Test
+    public void find_highest_in_array_of_two_descending_expect_first_element(){
+
+        // arrange
+        int input[] = {13, 7};
+        HighestNumberFinder cut = new HighestNumberFinder();
+        int expectedResult = 13;
+
+        // act
+        int actualResult = cut.findHighestNumber(input);
+
+        //assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void find_highest_in_array_of_two_ascending_expect_second_element(){
+
+        // arrange
+        int input[] = {7, 13};
+        HighestNumberFinder cut = new HighestNumberFinder();
+        int expectedResult = 13;
+
+        // act
+        int actualResult = cut.findHighestNumber(input);
+
+        //assert
+        assertEquals(expectedResult, actualResult);
+
+    }
+
 }
